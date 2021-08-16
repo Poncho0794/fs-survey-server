c4a2990ef0294b33aad06687eeff39477e2635d2
Alfonso Banda <alfonso@yalochat.com>
2021-08-16 00:05:17 -0500

    Finish Google OAuth section

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+require('dotenv').config()
 const express  = require('express');
+const passport = require('passport')
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+     console.log({ accessToken, refreshToken, profile })
+  })
+)
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
